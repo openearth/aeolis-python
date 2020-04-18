f210b4c94c8817daa7b3d196baad42a5afc8ec66
Sierd <sierd.devries@tudelft.nl>
2020-04-18 20:10:45 +0200

    small patch NETCDF4 enabled
    model settings are not stored in NETCDF anymore.
    
    Model Runs newest NETCDF package.
    
    readme updated

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url='http://aeolis.readthedocs.io/',
     license='GNU GPLv3',
     description='A process-based model for simulating supply-limited aeolian sediment transport',
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         'docopt==0.6.1',
         'bmi-python',
-	'netCDF4==1.4.1',
+	    'netCDF4',
         'scipy',
         'numpy',
     ],
