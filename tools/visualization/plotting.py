8f8bcec8b0b7c0967f6f5bcce45f4ae13db18fcd
bartvanwesten <bart.v.w@live.nl>
2022-03-22 09:47:55 +0100

    Changed plotting file

--- tools/visualization/plotting.py
+++ tools/visualization/plotting.py
@@ -15,7 +15,7 @@ from scipy.ndimage import gaussian_filter, uniform_filter
 
 
 
-def plot2d(ncfile, itimes, param='zb', cmap=plt.cm.jet, clim='auto'):
+def plot2d(ncfile, itimes=0, ifrac=0, param='zb', cmap=plt.cm.jet, clim='auto', delta=False, itimeDelta=0):
     
     '''Generic function for plotting 2D parameters from AeoLiS netcdf results
 
@@ -24,15 +24,21 @@ def plot2d(ncfile, itimes, param='zb', cmap=plt.cm.jet, clim='auto'):
     ncfile : str
         filename of the netcdf-file containing the results
     itimes : integer or array of integers
-        integer or list of integers describing the timestep-indices in the netcdf-file for plotting
-    param:
+        index or list of indices describing the timestep-indices in the netcdf-file for plotting
+    ifrac : integer
+        index of fraction, only used when the results has multiple dimensions ( >=4 )
+    param :
         string  describing the name of the parameter for plotting
-    cmap: colormap
+    cmap : colormap
         matplotlib colormap used for the plot
-    clim: list or string
+    clim : list or string
         if 'auto': the min and max values from the chosen parameter are used.
         else the min and max value can be given manually for the colorlimits of the colormap
-    quiver
+    delta : Bool (True or False)
+        Boolean for indicating plotting difference between timesteps or the actual value
+    itimeDelta : integer
+        index for the starting timestep in the netcdf-file when merging, only used when delta == Bool
+    quiver?
 
     Returns
     -------
@@ -56,10 +62,31 @@ def plot2d(ncfile, itimes, param='zb', cmap=plt.cm.jet, clim='auto'):
             # get spatial dimensions and bed levels
             x = ds.variables['x'][:,:]
             y = ds.variables['y'][:,:]
-            val = ds.variables[param][it, :, :]
-            v = -ds.variables[parameter_quivy][time_index, ::d, ::d]
+            
+            # Taking delta or acutal value 
+            if delta == True:
+                val = ds.variables[param][itimeDelta, ::] - ds.variables[param][it, ::]
+            else:
+                val = ds.variables[param][it, ::]
+                
+            # Selecting fractions if included
+            if len(val.shape) == 3:
+                val = val[:, :, ifrac]
+            
+            # Determining clim
+            if clim == 'auto':
+                vmin = np.min(val)
+                vmax = np.max(val)
+            else:
+                vmin = clim[0]
+                vmax = clim[1]
+                
+            # Plotting
+            fig, ax = plt.subplots()
+            ax.pcolormesh(x, y, val, cmap=cmap, vmin=vmin, vmax=vmax)
+            ax.colorbar()
     
-    return
+    return fig, ax
 
 
     
