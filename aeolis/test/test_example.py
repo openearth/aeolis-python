85d4b549181fc7719b7994ab7252e85133c83b43
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-02-08 10:21:00 +0100

    add test examples

---
+++ aeolis/test/test_example.py
@@ -0,0 +1,16 @@
+from aeolis.utils import normalize
+import numpy as np
+import pytest
+
+from pytest import approx
+
+def test_case1():
+    assert 5 == 5
+
+
+def test_case2():
+    assert 4 == 5
+
+
+def test_case3():
+    assert 5 != 5
