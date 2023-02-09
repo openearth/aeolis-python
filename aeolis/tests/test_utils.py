ce5eb70ce62cbc4503291fa715338e40c133983f
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-02-09 11:40:01 +0100

    rename test folder to 'tests'

---
+++ aeolis/tests/test_utils.py
@@ -0,0 +1,9 @@
+"""
+This file is part of AeoLiS test suit. 
+It uses the Pytest framework.
+"""
+
+from aeolis.model import StreamFormatter
+# from aeolis.utils import normalize
+# import numpy as np
+# import pytest
