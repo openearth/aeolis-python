cfb31808bbe2c5c1d0029b4be187e26b04820521
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-04-05 14:00:31 +0200

    implement test for StreamFormatter. Fixes #104

--- aeolis/tests/test_model.py
+++ aeolis/tests/test_model.py
@@ -17,3 +17,33 @@ To run an specific test, use:
     - If not in any group:
         pytest aeolis/tests/test_utils.py::<test-function>
 """
+
+import logging
+from aeolis.model import StreamFormatter
+
+class TestStreamFormatter:
+    """Test the stream formatter for console output"""
+
+    def test_stream_formatter_parent(self):
+        """Test if the stream formatter inherits from the logging.Formatter class"""
+        stream_formatter = StreamFormatter()
+        assert isinstance(stream_formatter, logging.Formatter)
+    
+    def test_stream_formatter_info_level(self):
+        """Test if the stream formatter returns log message for the info level"""
+        logger = logging.getLogger("Test")
+        info_record = logger.makeRecord("Test", logging.INFO, 
+                                        "Test", 1, "This is a message for INFO level",
+                                        None, None)
+        stream_formatter = StreamFormatter()
+        info_message = stream_formatter.format(info_record)
+        
+        warning_record = logger.makeRecord("Test warning", logging.WARNING,
+                                        "Test", 2, "This is a message for WARNING level",
+                                        None, None)
+
+        warning_message = stream_formatter.format(warning_record)
+
+        assert info_message == "This is a message for INFO level"
+        assert warning_message == "WARNING: This is a message for WARNING level"
+
