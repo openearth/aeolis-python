613c116b61f7d1dcb8929da6ac82e27bbf78f8f7
Niket <15953349+niketagrawal@users.noreply.github.com>
2023-10-06 14:03:16 +0200

    basic test for checking missing run command

--- aeolis/tests/test_console.py
+++ aeolis/tests/test_console.py
@@ -18,8 +18,9 @@ To run an specific test, use:
         pytest aeolis/tests/test_utils.py::<test-function>
 """
 
-from aeolis.console import aeolis_app
+from aeolis.console import aeolis_app, start_aeolis_app
 import unittest
+from unittest.mock import patch
 import shutil
 from typer.testing import CliRunner
 import os
@@ -37,53 +38,46 @@ class TestAeolisCommandLineInterface(unittest.TestCase):
         self.assertEqual(result.exit_code, 0)
 
     def test_run_command(self):
-        model_config_file_dir = os.path.join(
+        """
+        Test if aeolis run <path_to_configfile> command runs the simulation as expected.
+
+        Use the model configuration file in regression_tests/inputs/1D/case1_small_waves directory as the test input.
+
+        To verify the test, check if the model produces a log file and a netCDF file in the same directory as the model configuration file.
+
+        """
+        path_model_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
             "regression_tests",
             "inputs",
             "1D",
             "case1_small_waves",
         )
 
-        # path to parent directory
-        cwd = os.path.dirname(os.path.abspath(__file__))
+        path_model_config_file = os.path.join(path_model_dir, "aeolis.txt")
 
         result = runner.invoke(
             aeolis_app,
-            ["run", cwd + "/" + model_config_file_dir + "/aeolis.txt"],
+            ["run", path_model_config_file],
         )
 
         self.assertEqual(result.exit_code, 0)
 
-        # check if aeolis.log and aeolis.nc got created in model config file path
-        self.assertTrue(
-            os.path.isfile(
-                os.path.join(cwd, model_config_file_dir, "aeolis.log")
-            )
-        )
-        self.assertTrue(
-            os.path.isfile(
-                os.path.join(cwd, model_config_file_dir, "aeolis.nc")
-            )
-        )
-        # delete the generate log and nc files
-        os.remove(os.path.join(cwd, model_config_file_dir, "aeolis.log"))
-        os.remove(os.path.join(cwd, model_config_file_dir, "aeolis.nc"))
+        # Check if aeolis.log and aeolis.nc got generated in the directory containing the model configuration file
+        self.assertTrue(os.path.isfile(path_model_dir + "/aeolis.log"))
+        self.assertTrue(os.path.isfile(path_model_dir + "/aeolis.nc"))
 
-    # def test_missing_run_command(self):
-    #     model_config_file_dir = os.path.join(
-    #         "regression_tests",
-    #         "inputs",
-    #         "1D",
-    #         "case1_small_waves",
-    #     )
-    #     result = runner.invoke(
-    #         aeolis_app, [model_config_file_dir + "/aeolis.txt"]
-    #     )
+        # delete the generated aeolis.log and aeolis.nc files
+        os.remove(path_model_dir + "/aeolis.log")
+        os.remove(path_model_dir + "/aeolis.nc")
 
-    #     self.assertIn(
-    #         "Error: You entered an incorrect command.\n To run a model,",
-    #         result.stdout,
-    #     )
+    def test_run_command_with_invalid_config_file(self):
+        result = runner.invoke(
+            aeolis_app,
+            ["run", "invalid_config_file.txt"],
+        )
+
+        self.assertEqual(result.exit_code, 1)
 
     def test_examples_command(self):
         # delete existing aeolis-examples directory if it exists
@@ -98,6 +92,27 @@ class TestAeolisCommandLineInterface(unittest.TestCase):
 
         # do cleanup;delete the aeolis-examples directory
         shutil.rmtree("aeolis-examples")
+
+    def test_start_aeolis_app(self):
+        """
+            Test case: Execute aeolis/console.py as a script with only two arguments, for example python aeolis/console.py aeolis <path_config file>. 1st argument is aeolis and 2nd argument is a path to a model configuration file. DO not use runner invoke. Use mock patch to provide dummy arguments
+        assert for the print message in stdout: Following message should get printed on console ""Error: You entered an incorrect command.\n To run a model, type:"
+                " aeolis run <path_to_configfile>\n From aeolis v2.2.0 onwards, the"
+                " command run needs to be passed to run a model.\n""
+        """
+        path_model_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "regression_tests",
+            "inputs",
+            "1D",
+            "case1_small_waves",
+        )
+
+        path_model_config_file = os.path.join(path_model_dir, "aeolis.txt")
+
+        with patch("sys.argv", ["aeolis", path_model_config_file]):
+            start_aeolis_app()
+            self.assertNotEqual(start_aeolis_app(), 0)
 
 
 if __name__ == "__main__":
--- aeolis/tests/test_console_debug.py
+++ aeolis/tests/test_console_debug.py
@@ -1,38 +1,100 @@
-import os
-from aeolis.console import aeolis_app
-from typer.testing import CliRunner
+# import os
+# from aeolis.console import aeolis_app, start_aeolis_app
+# from unittest.mock import patch
+# import unittest
+# from typer.testing import CliRunner
+# from io import StringIO
 
-runner = CliRunner()
-
-
-def debug_run_command():
-    model_config_file_dir = os.path.join(
-        "regression_tests", "inputs", "1D", "case1_small_waves"
-    )
-    abs_path = os.path.dirname(os.path.abspath(__file__))
-
-    path = abs_path + "/" + model_config_file_dir + "/aeolis.txt"
-    print(path)
-    # run the model
-    result = runner.invoke(
-        aeolis_app,
-        ["run", abs_path + "/" + model_config_file_dir + "/aeolis.txt"],
-    )
-
-    print(result.stdout)
-
-    # # check if aeolis.log and aeolis.nc got created in model config file path
-    # self.assertTrue(
-    #     os.path.isfile(os.path.join(model_config_file_path, "aeolis.log"))
-    # )
-    # self.assertTrue(
-    #     os.path.isfile(os.path.join(model_config_file_path, "aeolis.nc"))
-    # )
-
-    # delete the generate log and nc files
-    os.remove(os.path.join(abs_path, model_config_file_dir, "aeolis.log"))
-    os.remove(os.path.join(abs_path, model_config_file_dir, "aeolis.nc"))
+# runner = CliRunner()
 
 
-if __name__ == "__main__":
-    debug_run_command()
+# def debug_run_command():
+#     path_model_config_file = os.path.join(
+#         os.path.dirname(os.path.abspath(__file__)),
+#         "regression_tests",
+#         "inputs",
+#         "1D",
+#         "case1_small_waves",
+#         "aeolis.txt",
+#     )
+
+#     print(path_model_config_file)
+
+#     # run the model
+#     result = runner.invoke(
+#         aeolis_app,
+#         ["run", path_model_config_file],
+#     )
+
+#     print(result.stdout)
+
+#     # check if the model produced a log file and a netCDF file
+#     assert os.path.isfile(
+#         os.path.join(
+#             os.path.dirname(os.path.abspath(__file__)),
+#             "regression_tests",
+#             "inputs",
+#             "1D",
+#             "case1_small_waves",
+#             "aeolis.log",
+#         )
+#     )
+#     assert os.path.isfile(
+#         os.path.join(
+#             os.path.dirname(os.path.abspath(__file__)),
+#             "regression_tests",
+#             "inputs",
+#             "1D",
+#             "case1_small_waves",
+#             "aeolis.nc",
+#         )
+#     )
+
+#     # delete the generated log and nc files
+#     os.remove(
+#         os.path.join(
+#             os.path.dirname(os.path.abspath(__file__)),
+#             "regression_tests",
+#             "inputs",
+#             "1D",
+#             "case1_small_waves",
+#             "aeolis.log",
+#         )
+#     )
+#     os.remove(
+#         os.path.join(
+#             os.path.dirname(os.path.abspath(__file__)),
+#             "regression_tests",
+#             "inputs",
+#             "1D",
+#             "case1_small_waves",
+#             "aeolis.nc",
+#         )
+#     )
+
+
+# class TestConsole(unittest.TestCase):
+#     def test_start_aeolis_app(self):
+#         """
+#             Test case: Execute aeolis/console.py as a script with only two arguments, for example python aeolis/console.py aeolis <path_config file>. 1st argument is aeolis and 2nd argument is a path to a model configuration file. DO not use runner invoke. Use mock patch to provide dummy arguments
+#         assert for the print message in stdout: Following message should get printed on console ""Error: You entered an incorrect command.\n To run a model, type:"
+#                 " aeolis run <path_to_configfile>\n From aeolis v2.2.0 onwards, the"
+#                 " command run needs to be passed to run a model.\n""
+#         """
+#         path_model_dir = os.path.join(
+#             os.path.dirname(os.path.abspath(__file__)),
+#             "regression_tests",
+#             "inputs",
+#             "1D",
+#             "case1_small_waves",
+#         )
+
+#         path_model_config_file = os.path.join(path_model_dir, "aeolis.txt")
+
+#         with patch("sys.argv", ["aeolis", path_model_config_file]):
+#             start_aeolis_app()
+#             self.assertNotEqual(start_aeolis_app(), 0)
+
+
+# if __name__ == "__main__":
+#     unittest.main()
