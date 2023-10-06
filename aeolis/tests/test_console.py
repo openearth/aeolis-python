4eaed68a0f5182d7745bd542abed36f36134dfb7
Niket <15953349+niketagrawal@users.noreply.github.com>
2023-10-06 20:09:43 +0200

    do assert operation inside context manager because calling function raises exception

--- aeolis/tests/test_console.py
+++ aeolis/tests/test_console.py
@@ -20,6 +20,8 @@ To run an specific test, use:
 
 from aeolis.console import aeolis_app, start_aeolis_app
 import unittest
+import sys
+from io import StringIO
 from unittest.mock import patch
 import shutil
 from typer.testing import CliRunner
@@ -39,11 +41,11 @@ class TestAeolisCommandLineInterface(unittest.TestCase):
 
     def test_run_command(self):
         """
-        Test if aeolis run <path_to_configfile> command runs the simulation as expected.
+        Test if executing `aeolis run <path_to_configfile>` on the command line runs the simulaition successfully.
 
         Use the model configuration file in regression_tests/inputs/1D/case1_small_waves directory as the test input.
 
-        To verify the test, check if the model produces a log file and a netCDF file in the same directory as the model configuration file.
+        To verify if simulation was completed successfully, check if aeolis.log file and a aeolis.nc file are generated in the same directory as the model configuration file.
 
         """
         path_model_dir = os.path.join(
@@ -90,15 +92,12 @@ class TestAeolisCommandLineInterface(unittest.TestCase):
         # test the creation of aeolis-examples directory in the same directory from where the command was run
         self.assertTrue(os.path.isdir("aeolis-examples"))
 
-        # do cleanup;delete the aeolis-examples directory
+        # Do cleanup: delete the aeolis-examples directory
         shutil.rmtree("aeolis-examples")
 
-    def test_start_aeolis_app(self):
+    def test_missing_run_command(self):
         """
-            Test case: Execute aeolis/console.py as a script with only two arguments, for example python aeolis/console.py aeolis <path_config file>. 1st argument is aeolis and 2nd argument is a path to a model configuration file. DO not use runner invoke. Use mock patch to provide dummy arguments
-        assert for the print message in stdout: Following message should get printed on console ""Error: You entered an incorrect command.\n To run a model, type:"
-                " aeolis run <path_to_configfile>\n From aeolis v2.2.0 onwards, the"
-                " command run needs to be passed to run a model.\n""
+        Test if executing `aeolis <path_to_configfile>` on the command line produces an error message telling the user to use `aeolis run <path_to_configfile>` instead.
         """
         path_model_dir = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
@@ -109,10 +108,19 @@ class TestAeolisCommandLineInterface(unittest.TestCase):
         )
 
         path_model_config_file = os.path.join(path_model_dir, "aeolis.txt")
-
-        with patch("sys.argv", ["aeolis", path_model_config_file]):
-            start_aeolis_app()
-            self.assertNotEqual(start_aeolis_app(), 0)
+        with patch("sys.argv", [path_model_config_file]):
+            with self.assertRaises(SystemExit) as cm:
+                result = start_aeolis_app()
+                self.assertNotEqual(cm.exception.code, 0)
+                self.assertIn(
+                    (
+                        "Error: You entered an incorrect command.\n To run a"
+                        " model, type: aeolis run <path_to_configfile>\n From"
+                        " aeolis v2.2.0 onwards, the command run needs to be"
+                        " passed to run a model.\n"
+                    ),
+                    result,
+                ),
 
 
 if __name__ == "__main__":
