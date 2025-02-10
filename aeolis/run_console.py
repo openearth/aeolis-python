39681da5bff98f25a6a86f29c0a3196e8342816a
Sierd de Vries <sierd.devries@tudelft.nl>
2025-02-10 17:17:06 +0100

    Bart fix (#234)
    * updated circular boundaries for sweep solver
    
    * Update utils.py
    
    * Added 4th generation sweep solver for testing

--- aeolis/run_console.py
+++ aeolis/run_console.py
@@ -6,8 +6,11 @@ def main()-> None:
     '''Runs AeoLiS model in debugging mode.'''
 
     # configfile = r'c:\Users\weste_bt\aeolis\Tests\RotatingWind\Barchan_Grid270\aeolis.txt'
-    configfile = r'C:\Users\svries\Documents\GitHub\OE_aeolis-python\aeolis\examples\grainsizevariations\aeolis_horizontalgradient1.txt'
+    configfile = r'C:\Users\svries\Documents\GitHub\Bart_mass\aeolis_duran.txt'
+    # configfile = r'C:\Users\svries\Documents\GitHub\Bart_mass\aeolis_windspeed.txt'
+
     aeolis_debug(configfile)
+
 
 if __name__ == '__main__':
     main()
