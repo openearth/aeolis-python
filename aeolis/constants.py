5db83bb91577eb1c38eb4b0a2f3bd4aadcf0d059
Sierd <sierd.devries@tudelft.nl>
2026-02-23 13:39:12 +0100

    updated constants.py and vegetation.py

--- aeolis/constants.py
+++ aeolis/constants.py
@@ -174,6 +174,7 @@ DEFAULT_CONFIG = {
     'process_moist'                 : False,              # Enable the process of moist
     'process_mixtoplayer'           : False,              # Enable the process of mixing 
     'process_wet_bed_reset'         : False,              # Enable the process of bed-reset in the intertidal zone
+    'process_sediment_supply'       : False,              # Enable the process of beach sediment supply
     'process_meteo'                 : False,              # Enable the process of meteo
     'process_salt'                  : False,              # Enable the process of salt
     'process_humidity'              : False,              # Enable the process of humidity
@@ -340,6 +341,10 @@ DEFAULT_CONFIG = {
     'rhoveg_max'                    : 0.5,                #maximum vegetation density, only used in duran and moore 14 formulation
     't_veg'                         : 3,                  #time scale of vegetation growth (days), only used in duran and moore 14 formulation
     'v_gam'                         : 1,                  # only used in duran and moore 14 formulation
+    'method_sed_supply'             :'vertical_beach_growth', # Name of method to comput sediment supply (wet_bed_reset, vertical_beach_growth, constant_SCR_constant_tanB, constant_SCR_variable_tanB)
+    'shoreline_change_rate'         : 0,                  # Elevation added to beach during process sed supply (shoreline change rate m/year)
+    'zshoreline'                    : 0,                  # Elevation where beach profile ends and shoreline change rate is applied           
+    'xshoreline'                    : 0,                  # Cross-shore position where beach profile ends and shoreline change rate is applied  
 }
 
 REQUIRED_CONFIG = ['nx', 'ny']
